69feea8cc06d66abb720b81a31f1a0d2c2ffbfa8
316Douji <511599737@qq.com>
2018-09-04 20:31:43 +0800

    trying sperate cubemap

---
+++ Assets/Scripts/TryCubemap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryCubemap : MonoBehaviour {
+    public ReflectionProbe rp;
+    public Camera cenCam;
+    public Cubemap testCubemap;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.H)) {
+            rp.RenderProbe();
+            this.GetComponent<Renderer>().material.SetTexture("_Cube", rp.bakedTexture);
+            print(rp.bakedTexture.name);
+            cenCam.RenderToCubemap(testCubemap);
+
+}
+	}
+}
