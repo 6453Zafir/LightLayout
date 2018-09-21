e6b02ea68efd74459bca0b66445027a0ea62710b
316Douji <511599737@qq.com>
2018-09-21 18:00:34 +0800

    模型整理，单一模型lightmap功能整合，还未成功

---
+++ Assets/Scripts/GetSingleLightmap.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GetSingleLightmap : MonoBehaviour {
+
+    public GameObject TestGO;
+
+    public RenderTexture RT;
+    public Texture2D Lightmap;
+    public Shader GetSingleLightmapShader;
+    // Use this for initialization
+    void Start () {
+        RT = new RenderTexture(1024, 1024, 24, RenderTextureFormat.ARGB32);
+        RT.enableRandomWrite = true;
+        RT.Create();
+    }
+    public Bounds CalcAABB(List<MeshRenderer> meshes)
+    {
+        var boxColliders = meshes.Select(x => x.bounds).ToList();
+        var aabb = boxColliders[0];
+        foreach (var obj in boxColliders)
+        {
+            aabb.Encapsulate(obj);
+        }
+        return aabb;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (TestGO == null) return;
+        var allRenderer = GameObject.FindObjectsOfType<MeshRenderer>();
+
+        foreach (var meshRenderer in allRenderer)
+        {
+            meshRenderer.gameObject.SetActive(false);
+        }
+
+        TestGO.SetActive(true);
+
+
+        var go = GameObject.Find("GBufferCamera");
+        Camera gBufferCamera = null;
+        if (go == null)
+            gBufferCamera = new GameObject("GBufferCamera").AddComponent<Camera>();
+        else
+            gBufferCamera = go.GetComponent<Camera>();
+        var aabb = CalcAABB(new List<MeshRenderer>() { TestGO.GetComponentInChildren<MeshRenderer>() });
+        var maxExtend = Mathf.Max(aabb.extents.x, aabb.extents.y, aabb.extents.z); ;
+        gBufferCamera.orthographic = true;
+        gBufferCamera.aspect = 1;
+        gBufferCamera.orthographicSize = maxExtend * 2;
+        gBufferCamera.allowMSAA = false;
+        gBufferCamera.allowHDR = false;
+
+        gBufferCamera.enabled = false;
+        gBufferCamera.clearFlags = CameraClearFlags.SolidColor;
+        gBufferCamera.backgroundColor = Color.clear;
+        gBufferCamera.farClipPlane = maxExtend * 2;
+        var cameraTransform = gBufferCamera.transform;
+        cameraTransform.position = aabb.center;
+        cameraTransform.forward = aabb.extents.x <= aabb.extents.y
+            ? (aabb.extents.x <= aabb.extents.z ? new Vector3(1, 0, 0) : new Vector3(0, 0, 1))
+            : (aabb.extents.y <= aabb.extents.z ? new Vector3(0, 1, 0) : new Vector3(0, 0, 1));
+        cameraTransform.position = aabb.center - cameraTransform.forward * maxExtend;
+        gBufferCamera.targetTexture = RT;
+        Shader.SetGlobalTexture("_Lightmap", Lightmap);
+        Shader.SetGlobalVector("_LightmspST", TestGO.GetComponent<MeshRenderer>().lightmapScaleOffset);
+        gBufferCamera.RenderWithShader(GetSingleLightmapShader, "");
+    }
+}
---
+++ Assets/Scripts/GetSingleLightmap.shader
@@ -0,0 +1,62 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced '_World2Object' with 'unity_WorldToObject'
+
+Shader "LightMapper/GetSingleLM"  
+{  
+    //属性  
+    Properties  
+    {
+    }  
+  
+    //子着色器    
+    SubShader  
+    {  
+        Pass  
+        {  
+		Cull Off
+		ZWrite Off
+			
+            CGPROGRAM  
+            //定义函数  
+            #pragma vertex vert  
+            #pragma fragment frag  
+			#include "UnityCG.cginc"
+            //定义结构体：应用阶段到vertex shader阶段的数据  
+            struct a2v  
+            {  
+                float4 vertex : POSITION;  
+				float4 uv2	: TEXCOORD1;
+            };  
+  
+            //定义结构体：vertex shader阶段输出的内容  
+            struct v2f  
+            {  
+                float4 pos : SV_POSITION; 			
+				float2 uv2 : TEXCOORD2;
+            };  
+			
+            //顶点shader  
+            v2f vert(a2v v)  
+            {  
+                v2f o;  
+                o.pos = float4(v.uv2.x * 2 - 1, v.uv2.y * -2 + 1, 0, 1);
+				o.uv2 = v.uv2;
+                return o;  
+            }  
+			sampler2D _Lightmap;
+			float4 _LightmspST;
+			#define pi 3.14159265358979 
+			
+            //片元shader  
+            fixed4 frag(v2f i) : SV_Target  
+            { 		
+
+				float4 color = tex2D(_Lightmap, i.uv2* _LightmspST.xy + _LightmspST.zw);
+				return float4(color.xyz, 1);
+            }  
+            ENDCG  
+        }  
+    }  
+} 
