3cf41d8d691c60e77cb590b1e1374b646a6c8fe7
316Douji <511599737@qq.com>
2018-09-27 21:27:50 +0800

    计算每个家具包围盒大成功

--- Assets/Scripts/GetSingleLightmap.cs
+++ Assets/Scripts/GetSingleLightmap.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class GetSingleLightmap : MonoBehaviour {
+public class GetSingleLightmap : MonoBehaviour
+{
 
-    public GameObject TestGO;
+    //public GameObject TestGO;
 
+    public GameObject FatherGO;
     public RenderTexture RT;
     public Texture2D Lightmap;
     public Shader GetSingleLightmapShader;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         RT = new RenderTexture(1024, 1024, 24, RenderTextureFormat.ARGB32);
         RT.enableRandomWrite = true;
         RT.Create();
@@ -51,18 +54,47 @@ public class GetSingleLightmap : MonoBehaviour {
     }
 
 
-// Update is called once per frame
-void Update () {
-        if (TestGO == null) return;
-        var allRenderer = GameObject.FindObjectsOfType<MeshRenderer>();
-
-        foreach (var meshRenderer in allRenderer)
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            meshRenderer.gameObject.SetActive(false);
+            for (int i = 0; i < FatherGO.transform.childCount; i++)
+            {
+                if (FatherGO.transform.GetChild(i).GetComponent<MeshRenderer>() != null)
+                {
+                    GameObject tempChild = FatherGO.transform.GetChild(i).gameObject;
+                    getSingleLightmap(tempChild);
+                }
+            }
+        }
+    }
+
+    Texture2D getSingleLightmap(GameObject ObToLightmap)
+    {
+
+        if (ObToLightmap == null) return null;
+        var LightMapTexture = new Texture2D(512, 512, TextureFormat.ARGB32, false);
+        var fillColorArray = LightMapTexture.GetPixels();
+
+        for (var i = 0; i < fillColorArray.Length; ++i)
+        {
+            fillColorArray[i] = Color.black;
         }
 
-        TestGO.SetActive(true);
+        LightMapTexture.SetPixels(fillColorArray);
 
+        LightMapTexture.Apply();
+
+
+        //var allRenderer = GameObject.FindObjectsOfType<MeshRenderer>();
+
+        //foreach (var meshRenderer in allRenderer)
+        //{
+        //    meshRenderer.gameObject.SetActive(false);
+        //}
+
+        //ObToLightmap.SetActive(true);
 
         var go = GameObject.Find("GBufferCamera");
         Camera gBufferCamera = null;
@@ -70,30 +102,38 @@ void Update () {
             gBufferCamera = new GameObject("GBufferCamera").AddComponent<Camera>();
         else
             gBufferCamera = go.GetComponent<Camera>();
-       // var aabb = CalcAABB(new List<MeshRenderer>() { TestGO.GetComponentInChildren<MeshRenderer>() });
-        //var aabb = CalcAABB(new List<MeshRenderer>() { TestGO.GetComponentsInChildren<MeshRenderer>()});
-        var aabb = CalcBounds(TestGO);
+        var aabb = CalcAABB(new List<MeshRenderer>() { ObToLightmap.GetComponent<MeshRenderer>() });
+        //var aabb = CalcBounds(TestGO);
         var maxExtend = Mathf.Max(aabb.extents.x, aabb.extents.y, aabb.extents.z); ;
         gBufferCamera.orthographic = true;
-        gBufferCamera.aspect = 1;
-        gBufferCamera.orthographicSize = maxExtend * 2;
+        //gBufferCamera.aspect = 1;
+        //gBufferCamera.orthographicSize = maxExtend * 2;
         gBufferCamera.allowMSAA = false;
         gBufferCamera.allowHDR = false;
 
         gBufferCamera.enabled = false;
         gBufferCamera.clearFlags = CameraClearFlags.SolidColor;
         gBufferCamera.backgroundColor = Color.clear;
-        gBufferCamera.farClipPlane = maxExtend * 2;
+        //gBufferCamera.farClipPlane = maxExtend * 2;
         var cameraTransform = gBufferCamera.transform;
-        cameraTransform.position = aabb.center;
-        cameraTransform.forward = aabb.extents.x <= aabb.extents.y
-            ? (aabb.extents.x <= aabb.extents.z ? new Vector3(1, 0, 0) : new Vector3(0, 0, 1))
-            : (aabb.extents.y <= aabb.extents.z ? new Vector3(0, 1, 0) : new Vector3(0, 0, 1));
-        cameraTransform.position = aabb.center - cameraTransform.forward * maxExtend;
+        //cameraTransform.position = aabb.center;
+        //cameraTransform.forward = aabb.extents.x <= aabb.extents.y
+        //    ? (aabb.extents.x <= aabb.extents.z ? new Vector3(1, 0, 0) : new Vector3(0, 0, 1))
+        //    : (aabb.extents.y <= aabb.extents.z ? new Vector3(0, 1, 0) : new Vector3(0, 0, 1));
+        //cameraTransform.position = aabb.center - cameraTransform.forward * maxExtend;
         gBufferCamera.targetTexture = RT;
         Shader.SetGlobalTexture("_Lightmap", Lightmap);
-        Shader.SetGlobalVector("_LightmspST", TestGO.GetComponentInChildren<MeshRenderer>().lightmapScaleOffset);
-        //Shader.SetGlobalVector("_LightmspST", TestGO.GetComponent<MeshRenderer>().lightmapScaleOffset);
+        //Shader.SetGlobalVector("_LightmspST", ObToLightmap.GetComponent<MeshRenderer>().lightmapScaleOffset);
         gBufferCamera.RenderWithShader(GetSingleLightmapShader, "");
+        return (ConvertRTtoT2D(RT));
+    }
+
+    Texture2D ConvertRTtoT2D(RenderTexture rt)
+    {
+        Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+        RenderTexture.active = rt;
+        tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        tex.Apply();
+        return tex;
     }
 }
--- Assets/Scripts/GetSingleLightmap.shader
+++ Assets/Scripts/GetSingleLightmap.shader
@@ -1,4 +1,6 @@
-﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+﻿// Upgrade NOTE: replaced tex2D unity_Lightmap with UNITY_SAMPLE_TEX2D
+
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
 
 // Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
 // Upgrade NOTE: replaced '_World2Object' with 'unity_WorldToObject'
@@ -36,24 +38,24 @@ Shader "LightMapper/GetSingleLM"
                 float4 pos : SV_POSITION; 			
 				float2 uv2 : TEXCOORD2;
             };  
-			
+			sampler2D _Lightmap;
+			float4 _LightmspST;
             //顶点shader  
             v2f vert(a2v v)  
             {  
                 v2f o;  
-                o.pos = float4(v.uv2.x * 2 - 1, v.uv2.y * -2 + 1, 0, 1);
+                o.pos = float4((v.uv2.x * unity_LightmapST.x + unity_LightmapST.z )* 2 - 1, (v.uv2.y*unity_LightmapST.y + unity_LightmapST.w) * -2 + 1, 0, 1);
 				o.uv2 = v.uv2;
                 return o;  
             }  
-			sampler2D _Lightmap;
-			float4 _LightmspST;
+
 			#define pi 3.14159265358979 
 			
             //片元shader  
             fixed4 frag(v2f i) : SV_Target  
             { 		
 
-				float4 color = tex2D(_Lightmap, i.uv2* _LightmspST.xy + _LightmspST.zw);
+				float4 color = tex2D(  _Lightmap , i.uv2* unity_LightmapST.xy + unity_LightmapST.zw);
 				return float4(color.xyz, 1);
             }  
             ENDCG  
